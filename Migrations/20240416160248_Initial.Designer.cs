797a33aeaf3c326116d734a0abcb322451fee400
David Caballero Savon <caballero.david2011@gmail.com>
2024-04-16 13:03:17 -0300

    the last commit

--- Migrations/20240416160248_Initial.Designer.cs
+++ Migrations/20240416160248_Initial.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace UserControl.Api.Authenticacao.Migrations
 {
     [DbContext(typeof(NetDevPackAppDbContext))]
-    [Migration("20240412032753_Initial")]
+    [Migration("20240416160248_Initial")]
     partial class Initial
     {
         /// <inheritdoc />
